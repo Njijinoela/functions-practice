678228a0ffa98a30f7ecfba645290549f78a3ecb
njiji <noelathandi123@gmail.com>
2024-10-02 20:51:23 +0300

    chess

--- chess.js
+++ chess.js
@@ -6,8 +6,25 @@ if(ax>0 && ax<=8 && ay>0 && ay<=8){
     allcordinates=allcordinates+`${ax},${ay} `
 }
 
+let bx = x+2;
+let by = y+1;
+if (bx>0 && bx<=8 && by>0 && by<=8){
+    allcordinates=allcordinates+`${bx},${by}`
+}
+
+let cx=x+2;
+let cy=y-1;
+if (cx>0 && cx<=8 && cy>0 && cy<=8){
+    allcordinates=allcordinates+`${cx},${cy}`
+}
+
+let dx = x+1
+let dy = y-2
+if (dx>0 && dx<=8 && dy>0 && dy<=8){
+    allcordinates=allcordinates+`${dx},${dy}`
+}
+
 console.log(allcordinates)
 
 }
 knightMoves(2,1);
-
