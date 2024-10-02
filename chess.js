a1885c6e6d69be4b70911686c33e45f33ac7e434
briankipyegon <kipyegonbrian201@gmail.com>
2024-10-02 16:13:44 +0300

    chess

--- chess.js
+++ chess.js
@@ -1,4 +1,12 @@
 function knightMoves(x, y){
+let ax=x+1
+let ay=y+2
+let allcordinates=""
+if(ax>0 && ax<=8 && ay>0 && ay<=8){
+    allcordinates=allcordinates+`${ax},${ay} `
+}
+
+console.log(allcordinates)
 
 }
 knightMoves(2,1);
